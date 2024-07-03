cc1f500cace5e8380aaed81adcedac1fb748e0f9
lyfsn <dev.wangyu@proton.me>
2024-07-03 17:16:37 +0800

    update

--- 09.send_msg.sh
+++ 09.send_msg.sh
@@ -19,13 +19,3 @@ hyperlane send message \
 
 
 
-
-hyperlane warp send \
-  --key $DEPLOYER_PRIVKEY \
-  --warp ~/.hyperlane/deployments/warp_routes/USDC/arbitrum-endurance-config.yaml \
-  --origin arbitrum \
-  --destination endurance
-
-
-
-
--- 10.send_warp.sh
+++ 10.send_warp.sh
@@ -1,25 +1,5 @@
 
 
-hyperlane send message \
-  --key $DEPLOYER_PRIVKEY \
-  --origin endurance \
-  --destination arbitrum \
-  --timeout 300 \
-  --verbosity info \
-  --body "1"
-
-
-hyperlane send message \
-  --key $DEPLOYER_PRIVKEY \
-  --origin arbitrum \
-  --destination endurance \
-  --timeout 300 \
-  --verbosity info \
-  --body "2" 
-
-
-
-
 hyperlane warp send \
   --key $DEPLOYER_PRIVKEY \
   --warp ~/.hyperlane/deployments/warp_routes/USDC/arbitrum-endurance-config.yaml \
