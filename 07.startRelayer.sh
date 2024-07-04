838e3fbc8e9584cc4c54e8b76c30c5c07ec432d7
lyfsn <dev.wangyu@proton.me>
2024-07-04 15:22:37 +0800

    add op address

---
+++ 07.startRelayer.sh
@@ -0,0 +1,60 @@
+
+
+export CONFIG_FILES=/root/hyperlane-deployer/agent.json
+export LAYER_DB_DIR=/root/hyperlane-deployer/relayer-db
+
+docker run \
+  -it \
+  -d \
+  -u root \
+  --name relayer-ace-arb \
+  -e CONFIG_FILES=/config/agent-config.json \
+  --mount type=bind,source=$CONFIG_FILES,target=/config/agent-config.json,readonly \
+  --mount type=bind,source=$LAYER_DB_DIR,target=/layer-db \
+  gcr.io/abacus-labs-dev/hyperlane-agent:main \
+  ./relayer \
+  --db /layer-db \
+  --relayChains endurance,arbitrum \
+  --defaultSigner.key $RELAYER_KEY \
+  --log.level info 
+
+
+export CONFIG_FILES=/root/hyperlane-deployer/agent.json
+export LAYER_DB_DIR=/root/hyperlane-deployer/relayer-db-2
+
+docker run \
+  -it \
+  -d \
+  -u root \
+  --name relayer-ace-arb \
+  -e CONFIG_FILES=/config/agent-config.json \
+  --mount type=bind,source=$CONFIG_FILES,target=/config/agent-config.json,readonly \
+  --mount type=bind,source=$LAYER_DB_DIR,target=/layer-db \
+  gcr.io/abacus-labs-dev/hyperlane-agent:main \
+  ./relayer \
+  --db /layer-db \
+  --relayChains endurance,arbitrum \
+  --defaultSigner.key $RELAYER_KEY \
+  --log.level info 
+
+
+
+
+export CONFIG_FILES=/root/hyperlane-deployer/agent.json
+export LAYER_DB_DIR=/root/hyperlane-deployer/relayer-db-op
+
+docker run \
+  -it \
+  -d \
+  -u root \
+  --name relayer-ace-op \
+  -e CONFIG_FILES=/config/agent-config.json \
+  --mount type=bind,source=$CONFIG_FILES,target=/config/agent-config.json,readonly \
+  --mount type=bind,source=$LAYER_DB_DIR,target=/layer-db \
+  gcr.io/abacus-labs-dev/hyperlane-agent:main \
+  ./relayer \
+  --db /layer-db \
+  --relayChains endurance,optimism \
+  --defaultSigner.key $RELAYER_KEY \
+  --log.level info 
+
--- 09.send_msg.sh
+++ 09.send_msg.sh
@@ -19,3 +19,22 @@ hyperlane send message \
 
 
 
+
+
+hyperlane send message \
+  --key $DEPLOYER_PRIVKEY \
+  --origin endurance \
+  --destination optimism \
+  --timeout 300 \
+  --verbosity info \
+  --body "1"
+
+
+hyperlane send message \
+  --key $DEPLOYER_PRIVKEY \
+  --origin optimism \
+  --destination endurance \
+  --timeout 300 \
+  --verbosity info \
+  --body "2" 
+
